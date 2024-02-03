10b9f4fe60fd907841ddfb708f5b86a22ff6ce4c
joaomalfatti <joao.cursos1210@gmail.com>
2024-02-03 10:25:21 -0400

    começando um projetinho utilizando future

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:consumo_servico_avancado/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
