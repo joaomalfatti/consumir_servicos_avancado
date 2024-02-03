fad8af9db4be58d7405e763aaccb3c6056f2cd07
joaomalfatti <joao.cursos1210@gmail.com>
2024-02-03 10:55:05 -0400

    Foi utilizado future e utilizando cases, uma interação melhor com o usuário

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   Future<Map> _recuperarPreco() async {
-    String url = "htpps://blockchain.info/ticker";
+    String url = "https://blockchain.info/ticker";
 
     http.Response response = await http.get(Uri.parse(url));
     return json.decode( response. body );
@@ -27,7 +27,35 @@ class _HomePageState extends State<HomePage> {
     return FutureBuilder<Map>(
       future: _recuperarPreco(),
       builder: (context, snapshot){
-        snapshot.
+
+
+        String resultado;
+
+        //Verificar se deu certo ou não.
+        switch( snapshot.connectionState ){
+             //Que defini o estado da conexão e null
+          case ConnectionState.none :
+             //Que aguarda a comunicação com API
+          case ConnectionState.waiting :
+            print("Conexao waiting");
+            resultado = "Carregando...";
+            break;
+             //Sem necessidade
+          case ConnectionState.active :
+             //Sem necessidade
+          case ConnectionState.done :
+          print("Conexao done");
+          if ( snapshot.hasError ){
+            resultado = "Erro ao carregar os dados";
+          }else {
+            double valor = snapshot.data! ["BRL"]["buy"];
+            resultado = "Preço do bitcoin: ${valor.toString()}";
+          };
+          break;
+        }
+        return Center(
+          child: Text( resultado )
+        );
       },
     );
   }
