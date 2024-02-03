ee0bad7f2035fd89fad06f2e0c8efbaa72d7d004
joaomalfatti <joao.cursos1210@gmail.com>
2024-02-03 10:37:04 -0400

    Foi criado método Future

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
 
 
 class HomePage extends StatefulWidget {
@@ -9,8 +13,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  Future<Map> _recuperarPreco() async {
+    String url = "htpps://blockchain.info/ticker";
+
+    http.Response response = await http.get(Uri.parse(url));
+    return json.decode( response. body );
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder<Map>(
+      future: _recuperarPreco(),
+      builder: (context, snapshot){
+        snapshot.
+      },
+    );
   }
 }
